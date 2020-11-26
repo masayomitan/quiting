7c0d48d1006bf70b23c34f7278d35a213315a31c
kawasaki <yomitanmasa3523@gmail.com>
2020-11-26 23:24:16 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,35 @@ const App: React.FC = () => {
     }
   };
 
+  return(
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>REACT QUIZ</h1>
+        { gameOver || userAnserw.length === TOTAL_QUESTIONS ?(
+          <button className='start' onClick={startTravia}>
+            start
+          </button>
+        ) : null }
+        { !gameOver ? <p className='score'>Score: {score}</p> : null }
+        { loading ? <p> Loading Questions...</p> : null }
+        {! loading && !gameOver && (
+          <QuestionCard
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </>
+  );
+};
 
-
-
-}
+export default App;
